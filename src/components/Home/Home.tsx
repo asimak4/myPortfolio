b4b348b880994b7dd1dfb17db7fee719ac790e5f
Alexander Simak <asimak4@gmail.com>
2024-09-09 12:09:37 -0400

    forcing update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -24,7 +24,7 @@ const Home: React.FC = () => {
         pt: 10,
         gap: 2, // Adds spacing between the text and image
       }}
-    >
+    > 
       <Box
         sx={{
           maxWidth: '800px',
