35de25c05676513387b817337b385a7e8ba15086
Alexander Simak <59295630+asimak4@users.noreply.github.com>
2025-07-09 16:45:51 -0400

    fixing key

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -349,7 +349,7 @@ const About: React.FC = () => {
                       <Grid item xs={12}>
                         <Box sx={{ mb: 2 }}>
                           <ReCAPTCHA
-                            sitekey="6LfBrZ8qAAAAAIT9rKc5hR-qkVBBOajLzA8WsFWH"
+                            sitekey="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"
                             onChange={handleCaptchaChange}
                           />
                         </Box>
